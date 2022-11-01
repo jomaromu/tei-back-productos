0ec5c7eba70fcbe3759447b6fb67660804c08537
Johnny Rosero <jomaromu2@gmail.com>
2022-11-01 12:14:30 -0500

    avances

--- dist/environment/environment.js
+++ dist/environment/environment.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.environmnet = void 0;
 exports.environmnet = {
-    port: Number(process.env.puerto) || 3005,
+    port: Number(process.env.puerto) || 4005,
     SEED: "todo-en-impresiones",
 };
--- environment/environment.ts
+++ environment/environment.ts
@@ -1,4 +1,4 @@
 export const environmnet = {
-  port: Number(process.env.puerto) || 3005,
+  port: Number(process.env.puerto) || 4005,
   SEED: "todo-en-impresiones",
 };
